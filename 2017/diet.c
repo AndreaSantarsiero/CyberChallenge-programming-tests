4b10984adcce6addb59f87f127da0997b655f5d3
AndreaSantarsiero <andreasantarsiero03@gmail.com>
2025-01-01 23:09:17 +0100

    continuo risoluzione parziale test 2017

--- 2017/diet.c
+++ 2017/diet.c
@@ -1,5 +1,7 @@
-//starting time: 23:26
-//after 1h 40min is not working. I think I unduerstood the problem
+//day 1 starting time: 23:26
+//after 1h 40min is not working.
+//day 2 starting time: 21:52
+//after 1h 10min is not working, but improved. official time finished
 
 #include <stdio.h>
 int scanfReturn = 0;
@@ -7,8 +9,9 @@ int scanfReturn = 0;
 
 
 
-void ordineDecrescente(int* array, int posizione, int arrayLenght);
+void ordineDecrescenteParziale(int* array, int posizione);
 int calcolaPesoTotale(int* array, int arrayLenght);
+int calcolaPotenziale(int* array, int arrayLenght, int posizione);
 void copiaArray(int* array, int* copia, int arrayLenght);
 void printOutput(int* array, int arrayLenght);
 int findNextPosizione(int posizione, int* array, int arrayLenght);
@@ -18,9 +21,8 @@ int findNextPosizione(int posizione, int* array, int arrayLenght);
 
 int main(int argc, char *argv[]){
     //inizializzazione
-    int sandwitchNumber = 0, pesoTotale = 0;
+    int sandwitchNumber = 0, maxPotenziale = 0;
     scanfReturn = scanf("%d", &sandwitchNumber);
-
 
     if(sandwitchNumber < 1 && sandwitchNumber > 10000){
         printf("invalid input\n");
@@ -29,17 +31,24 @@ int main(int argc, char *argv[]){
 
 
     int sandwitches[sandwitchNumber], temp[sandwitchNumber], solution[sandwitchNumber];
+
     for(int i = 0; i < sandwitchNumber; i++){
         scanfReturn = scanf("%d", &sandwitches[i]);
     }
 
+
     for(int i = 0; i < sandwitchNumber; i++){
-        copiaArray(sandwitches, temp, sandwitchNumber);
-        ordineDecrescente(temp, i, sandwitchNumber);
-        if(pesoTotale < calcolaPesoTotale(temp, sandwitchNumber)){
-            pesoTotale = calcolaPesoTotale(temp, sandwitchNumber);
-            copiaArray(temp, solution, sandwitchNumber);
+        maxPotenziale = 0;
+
+        for(int j = i; j < sandwitchNumber; j++){
+            copiaArray(sandwitches, temp, sandwitchNumber);
+            ordineDecrescenteParziale(temp, j);
+            if(calcolaPotenziale(temp, sandwitchNumber, j) > maxPotenziale){
+                maxPotenziale = calcolaPotenziale(temp, sandwitchNumber, j);
+                copiaArray(temp, solution, sandwitchNumber);
+            }
         }
+        sandwitches[i] = solution[i];
 
         /* printf("iterazione %d:\n", i);
         printOutput(solution, sandwitchNumber);
@@ -47,23 +56,24 @@ int main(int argc, char *argv[]){
     }
 
     
-    printOutput(solution, sandwitchNumber);
+    printOutput(sandwitches, sandwitchNumber);
     return 0;
 }
 
 
 
 
-void ordineDecrescente(int* array, int posizione, int arrayLenght){
-    int max = array[0];
+void ordineDecrescenteParziale(int* array, int posizione){
+    int max = 0;
 
-    for(int i = 0; i < posizione; i++){
-        if(array[i] < array[posizione]){
-            array[i] = 0;
+    for(int i = 0; i <= posizione; i++){
+        if(array[i] >= array[posizione]){
+            max = array[i];
+            break;
         }
     }
 
-    for(int i = 0; i < arrayLenght; i++){
+    for(int i = 0; i < posizione; i++){
         if(array[i] > max){
             array[i] = 0;
         }
@@ -71,6 +81,28 @@ void ordineDecrescente(int* array, int posizione, int arrayLenght){
             max = array[i];
         }
     }
+}
+
+
+int calcolaPotenziale(int* array, int arrayLenght, int posizione){
+    int potenziale = 0;
+
+    for(int i = 0; i < arrayLenght; i++){
+        if(i < posizione && array[i] > array[posizione]){
+            potenziale += array[i];
+        }
+        else if(i > posizione && array[i] < array[posizione]){
+            potenziale += array[i];
+        }
+        else if(i == posizione){
+            potenziale += array[i];
+        }
+        else{
+            array[i] = 0;
+        }
+    }
+
+    return potenziale;
 }
 
 
@@ -106,13 +138,8 @@ void printOutput(int* array, int arrayLenght){
     printf("%d\n", contatore);
 
     for(int i = 0; i < arrayLenght; i++){
-        if(array[i] > 0){   //
-            if((i+1) < arrayLenght){
-                printf("%d ", array[i]);
-            }
-            else{
-                printf("%d", array[i]);
-            }
+        if(array[i] > 0){   //array[i] > 0
+            printf("%d ", array[i]);
         }
     }
     printf("\n");
